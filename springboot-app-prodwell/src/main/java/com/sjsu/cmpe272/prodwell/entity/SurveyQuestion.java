f5bd4e6c3b633bbdbc5f1d0019080037c7085a85
Kenil Gopani <kenilgopani600@gmail.com>
2024-10-29 15:31:00 -0700

    get ServeyQuestionsAll issue is fixed, and refectoring of code

--- springboot-app-prodwell/src/main/java/com/sjsu/cmpe272/prodwell/entity/SurveyQuestion.java
+++ springboot-app-prodwell/src/main/java/com/sjsu/cmpe272/prodwell/entity/SurveyQuestion.java
@@ -18,40 +18,4 @@ public class SurveyQuestion {
     private int questionId;
     private String category;
     private String question;
-
-    // Getter and Setter for id
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    // Getter and Setter for questionId
-    public int getQuestionId() {
-        return questionId;
-    }
-
-    public void setQuestionId(int questionId) {
-        this.questionId = questionId;
-    }
-
-    // Getter and Setter for category
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    // Getter and Setter for question
-    public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
 }
