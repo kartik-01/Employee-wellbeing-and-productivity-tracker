99106565f1c8fb025a93fbf9884885b2fcc04a67
Kenil Gopani <kenilgopani600@gmail.com>
2024-11-27 14:15:12 -0800

    bulk quetions upload feature added

--- springboot-app-prodwell/src/main/java/com/sjsu/cmpe272/prodwell/entity/PersonalityAnswer.java
+++ springboot-app-prodwell/src/main/java/com/sjsu/cmpe272/prodwell/entity/PersonalityAnswer.java
@@ -1,7 +1,24 @@
 package com.sjsu.cmpe272.prodwell.entity;
 
+import java.time.LocalDateTime;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Document(collection = "personalityAnswers")
-public class PersonalityAnswer extends BaseSurveyAnswer {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonalityAnswer{
+    @Id
+    private ObjectId id;
+    private ObjectId questionId;
+    private ObjectId userId;
+    private String answer;
+    private LocalDateTime dateTime;
 }
