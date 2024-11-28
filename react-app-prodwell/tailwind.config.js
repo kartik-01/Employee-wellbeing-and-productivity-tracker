a5a3ebb8669093a5d064efc397404d8265f9f20e
yashsavani16 <yashbharatbhai.savani@sjsu.edu>
2024-11-27 19:22:48 -0800

    Tailwind configuration added

---
+++ react-app-prodwell/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// tailwind.config.js
+// module.exports = {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
