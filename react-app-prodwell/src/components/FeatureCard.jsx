b1bb32601d628a8911d7968c94da2843e20dc6b3
yashsavani16 <yashbharatbhai.savani@sjsu.edu>
2024-11-29 23:13:14 -0800

    Fixed image width and height in feature card component

--- react-app-prodwell/src/components/FeatureCard.jsx
+++ react-app-prodwell/src/components/FeatureCard.jsx
@@ -3,7 +3,8 @@ import React from "react";
 
 const FeatureCard = ({ imageSrc, title, description }) => (
   <div className="flex flex-col items-center text-center bg-white-100 p-6 rounded-lg shadow-md w-full md:w-1/3">
-    <img src={imageSrc} alt="feature image" className="mb-4" />
+    {/* <img src={imageSrc} alt="feature image" className="mb-4" /> */}
+    <img src={imageSrc} alt="feature image" className="mb-4 w-64 h-72 object-cover" />
     <h3 className="text-xl font-semibold text-gray-800 mb-2">{title}</h3>
     <p className="text-gray-600">{description}</p>
   </div>
