6f77d90652b73cb2e8b6db6b168852131564f65b
yashsavani16 <yashbharatbhai.savani@sjsu.edu>
2024-11-29 22:49:03 -0800

    Image path added in contact details

--- react-app-prodwell/src/components/ContactDetails.jsx
+++ react-app-prodwell/src/components/ContactDetails.jsx
@@ -5,18 +5,22 @@ const ContactDetails = () => (
   <section id="features" className="w-full bg-white py-16 px-8 text-center">
     <div className="flex flex-col md:flex-row md:space-x-8 space-y-8 md:space-y-0">
       <FeatureCard
+        imageSrc="/photos/male.png"
         title="Kenil Gopani"
         description="SJSU ID: 017992624"
       />
       <FeatureCard
+        imageSrc="/photos/yash.jpeg"
         title="Yash Savani"
         description="SJSU ID: 017581122"
       />
       <FeatureCard
+        imageSrc="/photos/male.png"
         title="Kartik Chindarkar"
         description="SJSU ID: 017583878"
       />
       <FeatureCard
+        imageSrc="/photos/female.jpg"
         title="Vyshnavi D P"
         description="SJSU ID: 018178979"
       />
