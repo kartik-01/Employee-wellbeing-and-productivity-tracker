a790cc932c4bd2f9f06b56e9fa9ffce1018589fc
yashsavani16 <yashbharatbhai.savani@sjsu.edu>
2024-11-29 22:48:08 -0800

    Image added in feature card

--- react-app-prodwell/src/components/FeatureCard.jsx
+++ react-app-prodwell/src/components/FeatureCard.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-const FeatureCard = ({ title, description }) => (
+
+const FeatureCard = ({ imageSrc, title, description }) => (
   <div className="flex flex-col items-center text-center bg-white-100 p-6 rounded-lg shadow-md w-full md:w-1/3">
+    <img src={imageSrc} alt="feature image" className="mb-4" />
     <h3 className="text-xl font-semibold text-gray-800 mb-2">{title}</h3>
     <p className="text-gray-600">{description}</p>
   </div>
