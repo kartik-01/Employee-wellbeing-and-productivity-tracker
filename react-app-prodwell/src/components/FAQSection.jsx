6d577d13ce9328ccea091d3566a21e2b4b422591
yashsavani16 <yashbharatbhai.savani@sjsu.edu>
2024-12-02 15:44:47 -0800

    Answer parameter added in FAQ Sections

--- react-app-prodwell/src/components/FAQSection.jsx
+++ react-app-prodwell/src/components/FAQSection.jsx
@@ -5,9 +5,9 @@ const FAQSection = () => (
   <section className="w-full bg-white-50 py-16 px-8">
     <h2 className="text-3xl font-semibold text-gray-800 text-center mb-8">FAQ</h2>
     <div className="space-y-4">
-      <FAQItem question="What is Framer?" />
-      <FAQItem question="Is it easy to learn?" />
-      <FAQItem question="Do I need to code?" />
+      <FAQItem question="What is Framer?" answer="All is well"/>
+      <FAQItem question="Is it easy to learn?" answer="All is well" />
+      <FAQItem question="Do I need to code?" answer="All is well"/>
     </div>
   </section>
 );
