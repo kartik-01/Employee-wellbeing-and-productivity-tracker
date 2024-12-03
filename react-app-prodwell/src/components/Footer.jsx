885b5a39f635b412105262c1c91235e42d3e916c
yashsavani16 <yashbharatbhai.savani@sjsu.edu>
2024-12-03 10:14:34 -0800

    Footer updated

--- react-app-prodwell/src/components/Footer.jsx
+++ react-app-prodwell/src/components/Footer.jsx
@@ -1,35 +1,33 @@
 import React from "react";
-import logo from '../assets/images/prodwell.png';
-import { Nav, Navbar, Button, Dropdown } from 'react-bootstrap';
+import logo from "../assets/images/prodwell.png";
 
 const Footer = () => {
   return (
     <div className="bg-gray-800 text-gray-300 py-6">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col md:flex-row justify-between items-center">
+        <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
           {/* Left Section */}
-          <div className="flex space-x-4 text-l">
+          <div className="text-center md:text-left">
             <p>&copy; {new Date().getFullYear()} ProdWell. All Rights Reserved.</p>
           </div>
           {/* Right Section */}
-          <div className="flex space-x-4 text-l">
-            <a href="" className="hover:text-white transition">
-              CMPE272
+          <div className="flex flex-col md:flex-row items-center md:space-x-4 text-center md:text-left space-y-4 md:space-y-0">
+            <div className="flex space-x-2">
+              <a href="#" className="hover:text-white transition">
+                CMPE272
+              </a>
+              <span>|</span>
+              <a href="#" className="hover:text-white transition">
+                Enterprise Software Platforms
+              </a>
+            </div>
+            <a href="/" className="flex items-center justify-center">
+              <img
+                src={logo}
+                alt="ProdWell logo"
+                className="w-8 h-8 rounded-full"
+              />
             </a>
-            <span>|</span>
-            <a href="" className="hover:text-white transition">
-              Enterprise Software Platforms
-            </a>
-            <Navbar.Brand href="/" className="mr-auto font-weight-bold">
-                <img
-                    src={logo}
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                    alt="ProdWell logo"
-                    style={{ borderRadius: "50%" }} 
-                />
-            </Navbar.Brand>
           </div>
         </div>
       </div>
