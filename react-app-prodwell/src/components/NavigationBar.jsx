4bf2a047eb4e2e9623699cdb5b38b832b84d6cc1
yashsavani16 <yashbharatbhai.savani@sjsu.edu>
2024-12-02 19:38:59 -0800

    Active navigation link added in navigation bar

--- react-app-prodwell/src/components/NavigationBar.jsx
+++ react-app-prodwell/src/components/NavigationBar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Nav, Navbar, Button, Dropdown } from 'react-bootstrap';
 import { useMsal } from '@azure/msal-react';
 import { loginRequest, b2cPolicies } from '../authConfig';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import './styles/NavigationBar.css';
 import logo from '../assets/images/prodwell.png'; // Adjust the path based on your actual file location
 import { EventType } from '@azure/msal-browser';
@@ -11,6 +11,7 @@ export const NavigationBar = () => {
     const { instance } = useMsal();
     const [userName, setUserName] = useState('');
     const navigate = useNavigate();
+    const location = useLocation();
 
     useEffect(() => {
         // Function to update userName based on active account
@@ -28,7 +29,7 @@ export const NavigationBar = () => {
 
         // Add event callback to listen for login success
         const callbackId = instance.addEventCallback((event) => {
-            if (event.eventType === EventType.LOGIN_SUCCESS || 
+            if (event.eventType === EventType.LOGIN_SUCCESS ||
                 event.eventType === EventType.ACQUIRE_TOKEN_SUCCESS) {
                 updateUserName(); // Update username on successful login
             }
@@ -65,48 +66,65 @@ export const NavigationBar = () => {
     };
 
     return (
-<Navbar bg="light" expand="lg" className="px-3 custom-navbar">
-<Navbar.Brand href="/" className="mr-auto font-weight-bold">
-    <img
-        src={logo}
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-        alt="ProdWell logo"
-        style={{ borderRadius: "50%" }} 
-    />
-    <span className="ml-2 mr-5">ProdWell</span>
-</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-            <Nav.Link onClick={() => navigate('/dashboard')}>Dashboard</Nav.Link>
-            <Nav.Link href="/aboutus">About Us</Nav.Link>
-            <Nav.Link onClick={()=>navigate('/survey')}>Survey</Nav.Link>
-        </Nav>
-        {userName ? (
-            <Dropdown align="end">
-                <Dropdown.Toggle variant="light" id="dropdown-basic" className="user-dropdown">
-                    {userName}
-                </Dropdown.Toggle>
+        <Navbar bg="light" expand="lg" className="px-3 custom-navbar">
+            <Navbar.Brand href="/" className="mr-auto font-weight-bold">
+                <img
+                    src={logo}
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    alt="ProdWell logo"
+                    style={{ borderRadius: "50%" }}
+                />
+                <span className="ml-2 mr-5">ProdWell</span>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link
+                        onClick={() => navigate('/dashboard')}
+                        className={`${location.pathname === "/dashboard"
+                                ? "bg-[#6200ea] text-white rounded-lg px-3"
+                                : "text-black"
+                            }`}
+                    >Dashboard</Nav.Link>
+                    <Nav.Link
+                        href="/aboutus"
+                        className={`${location.pathname === "/aboutus"
+                            ? "bg-[#6200ea] text-white rounded-lg px-3"
+                            : "text-black"
+                            }`}
+                    >About Us</Nav.Link>
+                    <Nav.Link
+                        onClick={() => navigate('/survey')}
+                        className={`${location.pathname === "/survey"
+                            ? "bg-[#6200ea] text-white rounded-lg px-3"
+                            : "text-black"
+                            }`}>Survey</Nav.Link>
+                </Nav>
+                {userName ? (
+                    <Dropdown align="end">
+                        <Dropdown.Toggle variant="light" id="dropdown-basic" className="user-dropdown">
+                            {userName}
+                        </Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                    <Dropdown.Item onClick={handleEditProfile}>Edit Profile</Dropdown.Item>
-                    <Dropdown.Item onClick={handleResetPassword}>Reset Password</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-        ) : (
-            <Button
-                variant="primary"
-                onClick={handleLoginSignUp}
-                className="ml-auto small-btn"
-            >
-                Login/Sign Up
-            </Button>
-        )}
-    </Navbar.Collapse>
-</Navbar>
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={handleEditProfile}>Edit Profile</Dropdown.Item>
+                            <Dropdown.Item onClick={handleResetPassword}>Reset Password</Dropdown.Item>
+                            <Dropdown.Divider />
+                            <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                ) : (
+                    <Button
+                        variant="primary"
+                        onClick={handleLoginSignUp}
+                        className="ml-auto small-btn"
+                    >
+                        Login/Sign Up
+                    </Button>
+                )}
+            </Navbar.Collapse>
+        </Navbar>
     );
 };
