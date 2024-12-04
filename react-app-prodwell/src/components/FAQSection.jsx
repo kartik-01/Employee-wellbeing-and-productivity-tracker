bd5167cf867afafc86846049ac8352c19c0169d8
yashsavani16 <yashbharatbhai.savani@sjsu.edu>
2024-12-04 13:08:18 -0800

    Accordion added in FAQ section

--- react-app-prodwell/src/components/FAQSection.jsx
+++ react-app-prodwell/src/components/FAQSection.jsx
@@ -5,9 +5,16 @@ const FAQSection = () => (
   <section className="w-full bg-white-50 py-16 px-8">
     <h2 className="text-3xl font-semibold text-gray-800 text-center mb-8">FAQ</h2>
     <div className="space-y-4">
-      <FAQItem question="What is Framer?" />
-      <FAQItem question="Is it easy to learn?" />
-      <FAQItem question="Do I need to code?" />
+      <FAQItem 
+        question="How does ProdWell enhance employee productivity and wellness?" 
+        answer="ProdWell uses AI to track productivity and wellness metrics, providing personalized insights and interventions that boost both employee well-being and performance."
+      />
+      <FAQItem question="What kind of personalized interventions does ProdWell offer?" 
+        answer="ProdWell provides tailored suggestions such as optimal break times and counseling, ensuring that each employee receives support suited to their unique needs."
+      />
+      <FAQItem question="Can I monitor employee metrics in real-time with ProdWell?"
+        answer="Yes, ProdWell offers real-time tracking of employee metrics, allowing you to stay updated on their productivity and wellness at any moment."
+      />
     </div>
   </section>
 );
