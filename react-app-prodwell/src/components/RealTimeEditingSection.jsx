af60d7591e73b9b52098a78274e090ee099464e4
yashsavani16 <yashbharatbhai.savani@sjsu.edu>
2024-12-04 13:06:21 -0800

    Image added in real time editing

--- react-app-prodwell/src/components/RealTimeEditingSection.jsx
+++ react-app-prodwell/src/components/RealTimeEditingSection.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import yash from "../assets/images/realTime.jpg"
 
 const RealTimeEditingSection = () => (
-  <section className="w-full bg-white-50 py-16 px-8 flex flex-col md:flex-row items-center">
-    <div className="md:w-1/2 text-center md:text-left">
-      <h2 className="text-3xl font-semibold text-gray-800 ml-4">Real-Time Editing</h2>
-      <p className="text-lg text-gray-600 mt-4 ml-4">Make edits in real-time, ensuring presentations are always updated & reflective of latest work.</p>
+  <section className="w-full bg-white-50 py-16 px-16 pl-28 flex flex-col md:flex-row items-center">
+    <div className="md:w-1/2 text-left md:text-left">
+      <h2 className="text-3xl font-semibold text-gray-800">Real-Time Editing</h2>
+      <p className="text-lg text-gray-600 mt-4">Make edits in real-time, ensuring presentations are always <br></br>updated & reflective of latest work.</p>
     </div>
     <div className="md:w-1/2 mt-8 md:mt-0">
-      <div className="bg-gray-200 h-48 w-full rounded-lg mr-4"></div>
+    <img src={yash} />
     </div>
   </section>
 );
